f4be43cadf2323dfde14b4b1fbf10e8f4c550fee
Marina Delinskaya <92973174+mdelinskaya@users.noreply.github.com>
2021-11-23 23:41:09 +0300

    Delete lesson5_task1.py

--- lesson5_task1.py
+++
@@ -1,7 +0,0 @@
-#Практическое занятие 5_Задача №1.
-
-with open(‘test.txt’, ‘w’) as file:
-    input_line = line(‘Text :\n’)
-    while input_line:
-        file.write(f’{input_line}\n’)
-        input_line = input(‘Text :\n’)
