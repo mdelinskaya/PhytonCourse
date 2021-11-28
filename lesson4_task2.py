b37a11e7f18f6f2af2cbc5f53a25216b79d4ac1a
Marina Delinskaya <92973174+mdelinskaya@users.noreply.github.com>
2021-11-28 16:32:44 +0300

    Delete lesson4_task2.py

--- lesson4_task2.py
+++
@@ -1,5 +0,0 @@
-#Урок 4 Задание 2.
-my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-length = len(my_list)
-new_list = [my_list[el] for el in range(1, length) if my_list[el] > my_list[el-1]]
-print(new_list)
